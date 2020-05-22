70f6661fe3c9c7ae045049a370e7d1ffd40edea4
Jonathan Rich <richj@oregonstate.edu>
2020-05-21 17:27:20 -0700

    Added scripts to package, addes routes for api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 //router.use('/XYZ', require('./XYZ'));//TODO: This
+router.use('/assignments', require('./assignments'));
+router.use('/courses', require('./courses'));
+router.use('/users', require('./users'));
 
 module.exports = router;
