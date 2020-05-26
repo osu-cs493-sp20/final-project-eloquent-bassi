0584f82e1f134743bfe4f0b7f7a906c906037037
richjoregonstate <richj@oregonstate.edu>
2020-05-26 16:31:29 -0700

    Added some db prototypes. They are a bit clunky with a bunch of _by_id but they should do the job.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-//router.use('/XYZ', require('./XYZ'));//TODO: This
 router.use('/assignments', require('./assignments'));
 router.use('/courses', require('./courses'));
 router.use('/users', require('./users'));
---
+++ storage/users_db.js
@@ -0,0 +1,13 @@
+const mysqlpool = require('../lib/mysqlPool')
+
+exports.create = async () => {//TODO: This
+    return
+}
+
+exports.login = async () => {//TODO: This is a placeholder for the query present in /lib/auth.js:8
+    return
+}
+
+exports.find_by_id = async (id) => {//TODO: This
+    return
+}
