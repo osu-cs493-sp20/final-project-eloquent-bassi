11525e158ff2ee4fff4c48b777ee50025ff074a0
Garishtech <crisdmann@gmail.com>
2020-06-11 00:44:50 -0700

    Rest of assignment-storage fleshed out. Yes, last two are repetitive

--- storage/assignments_db.js
+++ storage/assignments_db.js
@@ -9,14 +9,14 @@ exports.create = async (assignment) => {
         assignment.points,
         assignment.due
     ];
-    const [ result ] = await mysqlPool.query('INSERT INTO Assignment(courseId, title, points, due) ', params);
+    const [ result ] = await mysqlPool.query('INSERT INTO Assignment(course_id, title, points, due) VALUES(?,?,?,?);', params);
     return result.insertId;
 }
 
 exports.submit = async (submission, courseId) => {
     //Check if student is enrolled in the class
     const enrolled = await mysqlPool.query(`SELECT * FROM Enrolled WHERE 
-                                            studentId=${submission.studentId} AND 
+                                            student_id=${submission.studentId} AND 
                                             course_id=${courseId}`);
     if(enrolled){
         let params = [
@@ -25,7 +25,7 @@ exports.submit = async (submission, courseId) => {
             submission.timestamp,
             submission.file
         ];
-        const [ result ] = await mysqlPool.query('INSERT INTO Submission(assignmentId, studentId, timestamp, file', params); 
+        const [ result ] = await mysqlPool.query('INSERT INTO Submission(assignment_id, student_id, timestamp, file) VALUES(?,?,?,?);', params); 
         return result.insertId;
     }
     else{
@@ -33,24 +33,65 @@ exports.submit = async (submission, courseId) => {
     }
 }
 
-exports.find_by_id = async (id) => {//TODO: This
-    return
+exports.find_by_id = async (id) => {
+    const [result] = await mysqlPool.query('SELECT * FROM Assignment WHERE id= ?', id);
+    return result[0];
 }
 
-exports.update_by_id = async (id, assignment) => {//TODO: This
-    return
+exports.update_by_id = async (id, assignment) => {
+    const [result] = await mysqlPool.query('UPDATE SET ? WHERE assignment_id = ?', [assignment, id]);
+    return result;
 }
 
 exports.remove_by_id = async (id) => {//TODO: This
-    return
+    const [ result ] = await mysqlPool.query('DELETE * FROM Assignment WHERE assignment_id = ?', id);
+    return result;
 }
 
-exports.submissions_by_id = async (id, page) => {//TODO: This
-    return
+exports.submissions_by_id = async (id, page) => {
+    //Get count
+    const [ results ] = await mysqlPool.query(
+        'SELECT COUNT(*) AS count FROM Submissions');
+    const count = results[0].count;
+
+    const pageSize = 10;
+    const lastPage = Math.ceil(count / pageSize);
+    page = page > lastPage ? lastPage : page;
+    page = page < 1 ? 1 : page;
+    const offset = (page - 1) * pageSize;
+
+    const [ result ] = await mysqlPool.query('SELECT * FROM Submission WHERE assignment_id = ? ORDER BY id LIMIT ?,?', [id, offset, pageSize])
+    return {
+        submissions: result,
+        page: page,
+        totalPages: lastPage,
+        pageSize: pageSize,
+
+    }
 }
 
 exports.submissions_by_studentId = async (id, studentId, page) =>{//TODO: this
-    return 
+        //Get count
+    const [ results ] = await mysqlPool.query(
+        'SELECT COUNT(*) AS count FROM Submissions');
+    const count = results[0].count;
+
+    const pageSize = 10;
+    const lastPage = Math.ceil(count / pageSize);
+    page = page > lastPage ? lastPage : page;
+    page = page < 1 ? 1 : page;
+    const offset = (page - 1) * pageSize;
+
+    const [ result ] = await mysqlPool.query(
+        'SELECT * FROM Submission WHERE student_id = ? AND assignment_id = ? ORDER BY id LIMIT ?,?', 
+        [studentId, id, offset, pageSize]);
+    return {
+        submissions: result,
+        page: page,
+        totalPages: lastPage,
+        pageSize: pageSize,
+        
+    }
 }
 
 exports.update_submission_by_id = async (id) => {//TODO: This
