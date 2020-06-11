f208dc2ab0800528e8a095024b5cf263596c8335
Zachary Horine <zachary@horine.dev>
2020-06-10 19:53:01 -0700

    paginated courses get request

--- api/courses.js
+++ api/courses.js
@@ -1,11 +1,35 @@
 const router = require('express').Router();
 
+const { schemaAdd, schemaValidate } = require("../lib/validate");
 const courses_db = require('../storage/courses_db');
+
+const courseSchema = {
+  //TODO: Fill this in
+}
+
+schemaAdd(courseSchema);
 
 //==GET==
 router.get('/', async (req, res, next) => {//TODO: This
-    res.status(200).send("TBD")
-})
+  try {
+      const coursePage = await courses_db.get_page(parseInt(req.query.page) || 1);
+      coursePage.links = {};
+      if (coursePage.page < coursePage.totalPages) {
+        coursePage.links.nextPage = `/courses?page=${coursePage.page + 1}`;
+        coursePage.links.lastPage = `/courses?page=${coursePage.totalPages}`;
+      }
+      if (coursePage.page > 1) {
+        coursePage.links.prevPage = `/courses?page=${coursePage.page - 1}`;
+        coursePage.links.firstPage = '/courses?page=1';
+      }
+      res.status(200).send(coursePage);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: "Error fetching courses list.  Please try again later."
+    });
+  }
+});
 
 router.get('/:id', async (req, res, next) => {//TODO: This
     res.status(200).send("TBD")
