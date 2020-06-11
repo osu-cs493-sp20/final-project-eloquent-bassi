2472c767a5c0131b2f502acd00284438e2e73de4
Zachary Horine <zachary@horine.dev>
2020-06-10 19:14:06 -0700

    get_all, get_page, create, find_by_id

--- storage/courses_db.js
+++ storage/courses_db.js
@@ -1,37 +1,65 @@
 const mysqlpool = require('../lib/mysqlPool')
 
-exports.get_all = async () => {//TODO: This
+exports.get_all = async () => {
+  const [ result ] = await mysqlPool.query('SELECT * FROM Course');
+  return result;
+}
+
+exports.get_page = async (page) => {
+  const count = await getCourseCount();
+  const pageSize = 10;
+  const lastPage = Math.ceil(count / pageSize);
+  page = page > lastPage ? lastPage : page;
+  page = page < 1 ? 1 : page;
+  const offset = (page - 1) * pageSize;
+
+  const [ results ] = await mysqlPool.query('SELECT * FROM Course ORDER BY id LIMIT ?,?', [ offset, pageSize ] );
+  return {
+    courses: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: pageSize,
+    count: count
+  };
+}
+
+exports.create = async (course) => {
+  const [ result ] = await mysqlPool.query('INSERT INTO Course SET ?',course);
+  return result;
+}
+
+exports.find_by_id = async (id) => {
+  const [ result ] = await mysqlPool.query('SELECT * FROM Course WHERE course_id = ?;',id);
+  return result;
+}
+
+exports.update_by_id = async (id) => {
     return
 }
 
-exports.create = async () => {//TODO: This
+exports.remove_by_id = async (id) => {
     return
 }
 
-exports.find_by_id = async (id) => {//TODO: This
+exports.students_by_id = async (id) => {
     return
 }
 
-exports.update_by_id = async (id) => {//TODO: This
+exports.enroll_by_id = async (id) => {
     return
 }
 
-exports.remove_by_id = async (id) => {//TODO: This
+exports.roster_by_id = async (id) => {
     return
 }
 
-exports.students_by_id = async (id) => {//TODO: This
+exports.assignments_by_id = async (id) => {
     return
 }
 
-exports.enroll_by_id = async (id) => {//TODO: This
-    return
-}
-
-exports.roster_by_id = async (id) => {//TODO: This
-    return
-}
-
-exports.assignments_by_id = async (id) => {//TODO: This
-    return
+async function getCourseCount() {
+  const [ results ] = await mysqlPool.query(
+    'SELECT COUNT(*) AS count FROM Course'
+  );
+  return results[0].count;
 }
